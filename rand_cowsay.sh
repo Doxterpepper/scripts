3d117096203af0fe69f1fb6926760a212600435b
Dock O'Neal <docko@vmware.com>
2017-07-18 11:28:31 -0700

    Cross platform cowsay
    - The cows folder is in different places on different systems
    - When I get the oporotunity I can add a param for linux

--- rand_cowsay.sh
+++ rand_cowsay.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
-cow_path=/usr/local/Cellar/cowsay/3.04/share/cows/*.cow
+if [ $OSTYPE == "darwin16" ]
+then
+  cow_path=/usr/local/Cellar/cowsay/3.04/share/cows/*.cow
+fi
+#cow_path=/usr/local/Cellar/cowsay/3.04/share/cows/*.cow
 files=($(ls $cow_path))
 num=$[ 1 + $[RANDOM % 10 ]]
 cow=${files[$num]}
