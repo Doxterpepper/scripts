7c81b0449ba8a489b5c9b7c9fb3545358868aaa4
Dock O'Neal <docko@vmware.com>
2017-07-18 13:35:35 -0700

    Moved install alg to bash function

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
-bash_files=(mux.sh rand_cowsay.sh)
 install_path=/usr/local/bin
 
-for file in ${bash_files[@]}
-do
-  echo $file
-  chmod +x $file
-  # remove file extension
-  fn=$(echo $file | sed 's/\..*//')
-  ln $file $install_path/$fn
-done
+bash_files=(mux.sh rand_cowsay.sh)
+
+# Install scripts that use bash
+function bash_installs {
+  for file in ${bash_files[@]}
+  do
+    echo $file
+    chmod +x $file
+    # remove file extension
+    fn=$(echo $file | sed 's/\..*//')
+    ln $file $install_path/$fn
+  done 
+}
+
+bash_installs
+
